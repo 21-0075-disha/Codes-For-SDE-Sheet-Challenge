14f718e7416565acde696bc445e7f8ed47a2fa10
Disha Biswas <112744008+21-0075-disha@users.noreply.github.com>
2023-06-24 18:14:45 +0530

    Create Best Time to Buy and Sell Stock.cpp
    Problem 6

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+
+int maximumProfit(vector<int> &prices){
+
+      int maxProfit=0;
+
+      int mini=prices[0];
+
+      int cost=0;
+
+      for(int i=0;i<prices.size();i++){
+
+          cost=prices[i]-mini;
+
+          maxProfit=max(maxProfit,cost);
+
+          mini=min(prices[i],mini);
+
+      }
+
+      return maxProfit;
+
+}
